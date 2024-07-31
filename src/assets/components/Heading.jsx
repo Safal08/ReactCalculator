3e6b545bf4a227c6dac24b8e0b682633b27ab4be
safal08 <safalmahat5@gmail.com>
2024-07-31 19:35:57 +0300

    Add React Heading

--- src/assets/components/Heading.jsx
+++ src/assets/components/Heading.jsx
@@ -3,7 +3,7 @@ import styles from "./Heading.module.css";
 const Heading = () => {
   return (
     <div>
-      <h2 className={styles.heading}>Calulator</h2>
+      <h2 className={styles.heading}>React Calulator</h2>
     </div>
   );
 };
